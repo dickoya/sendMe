62f6de414c18a5e06750e82e416674e7dbeaf61c
IBRAHIMA <ib2diallo86@gmail.com>
2026-02-26 11:34:10 +0100

    Fist commit: configuration du projet

---
+++ src/main/java/dickoya/sendMe/sendMe/SendMeApplication.java
@@ -0,0 +1,13 @@
+package dickoya.sendMe.sendMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dickoya/sendMe/sendMe/SendMeApplicationTests.java
@@ -0,0 +1,13 @@
+package dickoya.sendMe.sendMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
