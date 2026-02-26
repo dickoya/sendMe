b7e0954ede71ec23a0628aaf712616d019944696
IBRAHIMA <ib2diallo86@gmail.com>
2026-02-26 21:46:40 +0100

    feat(api): ajouter un UserDTO de base

---
+++ api/src/main/java/dickoya/sendMe/DTO/UserDTO.java
@@ -0,0 +1,30 @@
+﻿package dickoya.sendMe.DTO;
+
+import dickoya.sendMe.models.Role;
+import dickoya.sendMe.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private Role role;
+
+    public static UserDTO fromUser(User user) {
+        return new UserDTO(
+            user.getId(),
+            user.getEmail(),
+            user.getFirstName(),
+            user.getLastName(),
+            user.getRole()
+        );
+    }
+}
